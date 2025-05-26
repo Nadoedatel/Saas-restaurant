9d3bebb8fc3984b13d06758ed4717972e78533de
Nikita <nikita.vorontsov.1989@mail.ru>
2025-05-26 15:03:19 +0300

    Сделал структуру раздделил на папки ui, layout, сделал новую страницу book a table для бронирования столика, а так же сделал отдельный header на все страницу через главный файл app

---
+++ apps/frontend/src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <nav class="flex flex-row gap-5 list-none">
+            <li>
+                 <RouterLink to="/">Главная</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/bookTable">Забронировать</RouterLink>
+            </li>
+            <li>
+                <RouterLink to="/about">О нас</RouterLink>
+            </li>
+        </nav>
+    </div>    
+</template>
