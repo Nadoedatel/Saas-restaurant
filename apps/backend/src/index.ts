effbe3d802b18dfa46b9a08c1f76a1b2d3735381
Nikita <nikita.vorontsov.1989@mail.ru>
2025-05-30 17:24:40 +0300

    Пытаюсь сделать qr-код и сделал страницу под это

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import bookingsRouter from './routes/bookings'
+import QRCode from 'qrcode';
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const qrUrl = `api/check-in/${booking.token}`;
+const qrCodeDataUrl = await QRCode.toDataURL(qrUrl);
 app.use(cors())
 app.use(express.json())
 app.use('/api/bookings', bookingsRouter)
