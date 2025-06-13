d88236db54194c468d41198cba70424d7db6384d
Nikita <nikita.vorontsov.1989@mail.ru>
2025-06-13 14:53:52 +0300

    Добавил vite-bundle-visualizer для проверки сколько весят страницы и общей картины происходящего

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { analyzer } from 'vite-bundle-analyzer'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+    analyzer(),
   ],
   resolve: {
     alias: {
